04607562e4bbe04ea41590a515ec9a029fa1c558
Oliver Eriksson <oliver.eriksson@elev.ga.lbs.se>
2019-01-10 10:57:28 +0100

    Init

---
+++ Assets/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public void LitRotate(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+
+}
